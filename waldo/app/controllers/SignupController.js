c5f58c8cadd7901a47aafe6897625e10f6fa022f
nrhubbar <nrhubbar@iupui.edu>
2017-11-07 17:50:33 -0800

    Implemented signup

--- waldo/app/controllers/SignupController.js
+++ waldo/app/controllers/SignupController.js
@@ -1,11 +1,14 @@
 let blueprint = require('@onehilltech/blueprint');
+let mongodb = require('@onehilltech/blueprint-mongodb')
 let util = require('util');
+let User = require('../models/User');
+let Profile = require('../models/Profile');
 
 
 /**
  * Registers the controller with Blueprint.
 */
-SignupController() {
+SignupController = function() {
     blueprint.BaseController.call (this);
 }
 
@@ -15,15 +18,43 @@ blueprint.controller (SignupController);
  * Returns a JSON containing signup information. 
 */
 SignupController.prototype.signup = () => {
-    
-    return 
-    {
-        data: {
-            name: String,
-            email: String,
-            password: String,
-            phoneNumber: String
-        }
+    const profileId = mongodb.Types.ObjectId();
+    return (req, res) => {
+        User.create({
+            name: req.body.data.name,
+            email: req.body.data.email,
+            password: req.body.data.password,
+            phoneNumber: req.body.data.phoneNumber,
+            profileId: profileId,
+            _id: mongodb.Types.ObjectId()
+        }).then((user, err) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            Profile.create({
+                _id: profileId,
+                isNull: true
+            }).then((profile, err) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.json({
+                    data: {
+                        //token: new Token() ???,
+                        id: user._id,
+                        type: String
+                    },
+                    relationships: {
+                        profile: {
+                            ...profile,
+                            id: profileId,
+                            type: "Profile"
+                        }
+                    }
+                })
+            })
+
+        })
     }
 }
 
