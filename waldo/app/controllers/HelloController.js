d3037f1380b9606804de0398a3f98da7c9931af7
herrejes <herrejespaj@gmail.com>
2017-11-07 20:04:43 -0500

    did a thing

---
+++ waldo/app/controllers/HelloController.js
@@ -0,0 +1,19 @@
+let blueprint = require('@onehilltech/blueprint');
+let util = require('util');
+
+HelloController = function() {
+    blueprint.Basecontroller.call(this);
+}
+
+blueprint.controller(HelloController);
+
+HelloController.prototype.hello = () => {
+    return (req, res) => {
+        res.json({
+            'greeting': "Hello World!"
+        });
+    }
+}
+
+
+module.exports = exports = HelloController;
