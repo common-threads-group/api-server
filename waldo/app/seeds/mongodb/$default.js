333364143d7266eb704748dc3717aa31a3d8203d
nrhubbar <nrhubbar@iupui.edu>
2017-11-13 18:39:46 -0800

    Added seed to include an account so I can get a test token

--- waldo/app/seeds/mongodb/$default.js
+++ waldo/app/seeds/mongodb/$default.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const dab = require ('@onehilltech/dab');
+const ObjectId = require ('@onehilltech/blueprint-mongodb').Types.ObjectId;
 
 const scopes = [
   ["*"],
@@ -17,13 +18,24 @@ module.exports = {
     let clientName = 'ember-waldo';
     let client = {
       _id: "5a02944ad05605078a17da82",
+      username: clientName,
       name: clientName,
       client_secret: "eqb16wYXmahv85m5NIPXeiJZDdXZscwlGGABPcBNO7a1mZj0rg_ZB9a_SM705Wcv5OMZVe7B1qO05i8SSjnfWMOAPkfI9n4gKCFeD8GHqu7Vacyhqr6O6bLB5hRXIec0wkpTYDZMSyZh6MbgZn_xhRHTEtIyhyZbNCXUNmWakEI",
       email: clientName + '@waldo.com',
+      password: "1234",
       scope: scopes[i],
       type: 'native'
     };
 
     return callback (null, client);
-  })
+  }),
+  accounts: [
+    {
+      _id: new ObjectId ('58ed92f64da0861f6aeb98cf'),
+      email: 'tester@waldo.com',
+      username: 'waldo',
+      password: 'waldo',
+      created_by: dab.ref ('clients.0')
+    }
+  ]
 };
