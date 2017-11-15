69a3de9858e7b22ddcc72df2d2c730ab5fdaaf2c
nrhubbar <nrhubbar@iupui.edu>
2017-11-15 12:44:55 -0800

    Added router for education

---
+++ waldo/app/routers/v1/EducationRouter.js
@@ -0,0 +1,14 @@
+module.exports = exports = {
+    '/education/:profileId' : {
+        post: {action : 'EducationController@add'},
+        get: {action : 'EducationController@getSingle'},
+        delete: {action : 'EducationController@delete'}
+
+    },
+    '/education/:educationId' : {
+        put: {action : 'EducationController@update'}
+    },
+    '/profile/education/:profileId': {
+        get: {}
+    }
+}
