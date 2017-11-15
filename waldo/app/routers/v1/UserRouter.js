9ee92504686b66bea4f128c9e2e846a7f447f594
nrhubbar <nrhubbar@iupui.edu>
2017-11-14 19:20:24 -0800

    Added cors to individual router

--- waldo/app/routers/v1/UserRouter.js
+++ waldo/app/routers/v1/UserRouter.js
@@ -1,5 +1,8 @@
+const cors = require('cors')
+
 module.exports = exports = {
     '/user/:accountId' : {
+        use: cors(),
         post : { action : 'UserController@create' },
         get : { action : 'UserController@get' }
     }
