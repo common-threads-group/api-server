e88e6ed93ec4c0c7c251a0e7a9a0ca9595a55eae
nrhubbar <nrhubbar@iupui.edu>
2017-11-15 13:02:14 -0800

    Implemented add education functionality

--- waldo/app/controllers/EducationController.js
+++ waldo/app/controllers/EducationController.js
@@ -10,7 +10,29 @@ class EducationController {
 
     add() {
         return (req, res) => {
-            
+            const newEducation = req.body.data.attributes;
+            Education.create({
+                title: newEducation.title,
+                schoolName: newEducation.schoolName,
+                city: newEducation.city,
+                state: newEducation.state,
+                startDate: newEducation.startDate,
+                endDate: newEducation.endDate,
+                about: newEducation.about,
+                profileId: req.params.profileId
+            }, (err, education) => {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json({
+                        data: {
+                            id: education._id,
+                            type: 'Education',
+                            attributes: education
+                        }
+                    });
+                }
+            });
         };
     }
 
