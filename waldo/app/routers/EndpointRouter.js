c852f107627012e56af8b8638679efbcd10e8f66
nrhubbar <nrhubbar@iupui.edu>
2017-11-14 19:08:25 -0800

    Used cors as single thing

--- waldo/app/routers/EndpointRouter.js
+++ waldo/app/routers/EndpointRouter.js
@@ -3,11 +3,13 @@
 const cors = require ('cors');
 const gatekeeper = require('@onehilltech/blueprint-gatekeeper');
 
+const corsOptions = {
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+};
+
 module.exports = {
   '/v1': {
-    use: [
-      cors (),
-    ],
+    use: cors (corsOptions),
     policy: 'gatekeeper.auth.bearer'
   }
 };
