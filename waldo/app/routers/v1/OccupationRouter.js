b7ed37e36db6ea2606ac0267f911ee7213ef1d33
Arin Mauk <arin.mauk7@gmail.com>
2017-11-19 15:05:28 -0500

    Finished implementing POST, PUT, GET for Occupation router.

---
+++ waldo/app/routers/v1/OccupationRouter.js
@@ -0,0 +1,12 @@
+module.exports = exports = {
+    '/occupation/:profileId' :{
+        post : { action : '' },
+        get : { action : '' }
+    },
+    '/occupation/:occupationId':{
+        put : { action : '' },
+        get : { action : '' },
+        delete : { action : '' }
+    }
+    
+}
